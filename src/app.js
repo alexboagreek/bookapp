465f5e91e4f4e631743e64e6bbc7e1b2c15c0e12
Alex <olivesargreek@gmail.com>
2023-02-18 12:57:30 +0300

    feat: добавлен AbstractView и выведен текст в app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-"use strict";
+import { MainView } from './views/main/main.js';
+class App {
+  routes = [
+    { path: "", view: MainView }
+  ];
+
+  constructor() {
+    window.addEventListener('hashchange', this.route.bind(this));
+    this.route();
+  }
+
+  route() {
+
+    if (this.currentView) {
+      this.currentView.destroy();
+    }
+
+    const view = this.route.find(route => route.path == location.hash).view;
+    this.currentView = new view();
+    this.currentView.render();
+  }
+}
+
+new App();
